9683aee9a0e762f8639c9e7d75694400468768d5
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2026-01-14 11:29:34 +0530

    added configurations
    added configurations

---
+++ README.md
@@ -0,0 +1,5 @@
+# E-Wallet-Cloud-Config
+E-Wallet-Cloud-Config service for E-Wallet App created by Bikram Dey
+
+# Annotations
+@EnableConfigServer -> to enable as config server
---
+++ src/main/java/com/bikram/E_Wallet/EWalletCloudConfigApplication.java
@@ -0,0 +1,15 @@
+package com.bikram.E_Wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EWalletCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletCloudConfigApplication.class, args);
+	}
+
+}
